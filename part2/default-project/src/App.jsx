c42627e00dcfc7ef4bf3cf7b84b31d1290ce3a20
unnamedpilot <aalab@eafit.edu.co>
2026-03-31 17:16:37 -0500

    feat(part2): finish exercises 2.6-2.10

--- part2/default-project/src/App.jsx
+++ part2/default-project/src/App.jsx
@@ -1,12 +1,49 @@
+import { useState } from "react"
 import Notes from "./components/Notes"
 
 const App = ({ notes }) => {
+  const [ notesState, setNotes ] = useState(notes)
+  const [ newNote, setNewNote ] = useState("...A new note")
+  const [ showAll, setShowAll ] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log(event.target);
+    const newRecord = {
+      id: notesState.length + 1,
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    console.log(newRecord);
+    
+    setNotes((prevNotes) => prevNotes.concat(newRecord))
+    setNewNote("")
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll 
+    ? notesState 
+    : notesState.filter((note) => note.important)
+
   
   return (
-    <>
-    <Notes notes={notes}/>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <Notes notes={notesToShow}/>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">submit</button>
+      </form>
+    </div>
   )
+
 }
 
 export default App
--- part2/default-project/src/components/Notes.jsx
+++ part2/default-project/src/components/Notes.jsx
@@ -1,4 +1,5 @@
 
+
 const Notes = ({ notes }) => {
     return (
         <div>
