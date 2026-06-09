364fbde630060f908d8c3b57182a0db32bcdaffc
unnamedpilot <aalab@eafit.edu.co>
2026-06-09 13:36:42 -0500

    feat: starts part3 and finishes exercises 3.1-3.8

---
+++ part3/default-server/index.js
@@ -0,0 +1,89 @@
+const express = require("express")
+const app = express()
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+function generateId(notes) {
+  if (notes.length > 0) {
+    const ids = notes.map((note) => parseInt(note.id))
+    return Math.max(...ids) + 1
+  }
+  return 0
+}
+
+console.log(generateId(notes))
+
+app.use(express.json())
+
+app.use((request, response, next) => {
+  console.log("Method: ", request.method)
+  console.log("Path: ", request.path)
+  console.log("Body: ", request.body)
+  console.log("---")
+  next()
+  
+})
+
+app.get("/", (request, response) => response.send("<h1>Hello World</h1>"))
+
+app.get("/api/notes", (request, response) => response.json(notes))
+
+app.get("/api/notes/:id", (request, response) => {
+  let id = request.params.id
+  let note = notes.find((n) => n.id === id)
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id
+  notes = notes.filter((n) => !(n.id === id))
+  response.status(204).end()
+})
+
+app.post("/api/notes", (request, response) => {
+
+  const body = request.body
+  if (!body.content) {
+    return response.status(400).json({
+      error: "The request doesn't have any content in the body"
+    })
+  }
+
+  const content = body.content
+  const important = Boolean(body.important)
+  const id = generateId(notes)
+
+  const new_note = { id, content, important }
+
+  notes.concat(new_note)
+
+  response.status(201).end()
+})
+
+app.use((request, response) => {
+  response.status(404).json({error: "unknown endpoint"})
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`The server is running in ${PORT}`))
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,106 @@
+function generateId() {
+    return parseInt(Math.random() * 1000000000000)
+}
+
+
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+
+
+app.use(express.json())
+
+app.use(morgan((tokens, request, response) => {
+    const item = request.method === "POST" 
+        ? JSON.stringify(request.body)
+        : "" 
+
+    return [
+        tokens.method(request, response), 
+        tokens.url(request, response),
+        tokens.status(request, response),
+        tokens.res(request, response, 'content-length'), "-",
+        tokens["response-time"](request, response),
+        item 
+    ].join(' ')
+}))
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons)
+})
+
+app.get("/info", (req, res) => {
+    const now = new Date()
+    const html_file = `<p>Phonebook has info for ${persons.length}</p> 
+        <p>${now.toString()}</p>`
+    res.contentType("html").send(html_file)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    const person = persons.find((p) => p.id === id)
+    if(person){
+        res.json(person)
+    }
+    res.status(200).end()
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = req.params.id
+    persons = persons.filter((person) => !(person.id === id))
+    res.status(204).end()
+})
+
+
+app.post("/api/persons", (req, res) => {
+    const body = req.body
+
+    const id = generateId()
+    const name = body.name
+    const number = body.number
+
+    if (!name || !number) {
+        return res.status(400).json({
+            error: "either name or number is missing"
+        })
+    }
+
+    if(persons.find((person) => person.name === name)) {
+        return res.status(409).json({
+            error: "name must be unique"
+        })
+    }
+
+    const new_person = { id, name, number }
+
+    persons.concat(new_person)
+    res.status(201).json(new_person)
+})
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {console.log(`The server is available in the PORT ${PORT}`)})
