c68fbc0d08acd31042169e7ef2642daeb1c7fe82
unnamedpilot <aalab@eafit.edu.co>
2026-06-14 16:21:25 -0500

    buildui

--- part3/phonebook/backend-phonebook/index.js
+++ part3/phonebook/backend-phonebook/index.js
@@ -11,6 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors("http://localhost:5173"))
+app.use(express.static("dist"))
 app.use(morgan((tokens, request, response) => {
     const item = request.method === "POST" 
         ? JSON.stringify(request.body)
