d211f27e7bf1b41dd135727fb9684a0d0eedc782
unnamedpilot <aalab@eafit.edu.co>
2026-06-14 16:24:55 -0500

    refactor: remove cors since back and front are at the same server

--- part3/phonebook/backend-phonebook/index.js
+++ part3/phonebook/backend-phonebook/index.js
@@ -5,12 +5,10 @@ function generateId() {
 
 const express = require("express")
 const morgan = require("morgan")
-const cors = require("cors")
 const app = express()
 
 
 app.use(express.json())
-app.use(cors("http://localhost:5173"))
 app.use(express.static("dist"))
 app.use(morgan((tokens, request, response) => {
     const item = request.method === "POST" 
