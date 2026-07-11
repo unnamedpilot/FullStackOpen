2279854f98469d025fb0eb0e7b658759103ca57b
UnnamedPilot <alverth.antonio@hotmail.com>
2026-07-11 01:30:16 -0500

    refactor: use index.js to connect to connect to db using database util instead of app.js

--- part4/backend-tutorial/index.js
+++ part4/backend-tutorial/index.js
@@ -1,6 +1,9 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const app = require('./app')
+const database = require('./utils/database')
+
+database.connect(config.MONGODB_URI)
 
 const PORT = config.PORT || 3001
 app.listen(PORT, () => logger.info(`The server is running in ${PORT}`))
