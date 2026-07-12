936562331a3ef92ab52c8055298edeb2484c46de
UnnamedPilot <alverth.antonio@hotmail.com>
2026-07-11 20:11:10 -0500

    feat: implement setup code using memory server and groups test using describe

--- part4/backend-tutorial/tests/note_api.test.js
+++ part4/backend-tutorial/tests/note_api.test.js
@@ -1,24 +1,66 @@
 const { MongoMemoryServer } = require('mongodb-memory-server')
-const { test, before, after } = require('node:test')
+const { test, describe, before, after } = require('node:test')
 const supertest = require('supertest')
 const app = require('../app')
 const database = require('../utils/database')
+const Note = require('../models/note')
+const logger = require('../utils/logger')
 
 let memoryServer
 
 before(async () => {
   memoryServer = await MongoMemoryServer.create()
   await database.connect(memoryServer.getUri())
+
+  const notes = [
+    {
+      content: 'HTML is a markup language.',
+      important: true,
+    },
+    {
+      content: 'CSS controls the presentation of a webpage.',
+      important: false,
+    },
+    {
+      content: 'JavaScript can modify the DOM.',
+      important: true,
+    },
+    {
+      content: 'Node.js allows JavaScript to run outside the browser.',
+      important: true,
+    },
+    {
+      content: 'Express is a web framework for Node.js.',
+      important: false,
+    },
+  ]
+
+  try {
+    logger.info('Inserting notes to Notes collection')
+    await Note.insertMany(notes)
+    logger.info('Notes inserted')
+
+  } catch (error) {
+    logger.error('Error inserting notes:', error.message)
+    process.exit(1)
+  }
+
 })
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
-  await api
-    .get('/api/notes')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('note api', () => {
+
+  test('notes are returned as json', async () => {
+    await api
+      .get('/api/notes')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
 })
+
+
 
 after(async () => {
   await database.disconnect()
